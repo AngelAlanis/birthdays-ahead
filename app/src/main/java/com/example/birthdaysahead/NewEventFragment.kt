e5c6a7fc902fc17c906d7c655b8ff4c17dd60d75
ahangel256@gmail.com <ahangel256@gmail.com>
2023-05-07 19:42:59 -0600

    Added MaterialDatePicker to NewEventFragment.kt

--- app/src/main/java/com/example/birthdaysahead/NewEventFragment.kt
+++ app/src/main/java/com/example/birthdaysahead/NewEventFragment.kt
@@ -6,6 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.birthdaysahead.databinding.NewEventLayoutBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class NewEventFragment : Fragment() {
@@ -20,4 +23,34 @@ class NewEventFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.dateEditText.setOnClickListener {
+            val calendar = Calendar.getInstance()
+
+            val materialDatePicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select the birthdate")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build()
+
+            materialDatePicker.addOnPositiveButtonClickListener {
+                val date = Date(it)
+                updateEditText(date)
+            }
+
+            materialDatePicker.show(requireActivity().supportFragmentManager, "materialDatePicker")
+        }
+    }
+
+    private fun updateEditText(date: Date) {
+        val format = "dd/MM/yyyy"
+        val formatter = SimpleDateFormat(format, Locale.getDefault())
+        formatter.timeZone = TimeZone.getTimeZone("UTC")
+
+        binding.dateEditText.setText(formatter.format(date).toString())
+    }
 }
