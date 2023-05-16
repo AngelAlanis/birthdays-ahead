9984f58ad57d161eb021c0be0d95c6f9f9a74e8c
ahangel256@gmail.com <ahangel256@gmail.com>
2023-05-15 18:04:27 -0600

    Added more fields to the Event.kt to handle notes, likes and dislikes, and wishes.

--- app/src/main/java/com/example/birthdaysahead/model/Event.kt
+++ app/src/main/java/com/example/birthdaysahead/model/Event.kt
@@ -6,7 +6,11 @@ data class Event(
     val name: String,
     val date: LocalDate,
     val color: Int,
-    val typeOfEvent: TypeOfEvent
+    val typeOfEvent: TypeOfEvent,
+    val note: String,
+    val giftWishes: List<String>,
+    val likes: List<String>,
+    val dislikes: List<String>
 ) {
     val iconChar: Char = name.first()
 }
