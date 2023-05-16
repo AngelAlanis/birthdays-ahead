451ef09bf5d6c1007fa1e715d42dbd5c31173c03
ahangel256@gmail.com <ahangel256@gmail.com>
2023-05-15 20:50:00 -0600

    Added properties.

--- app/src/main/java/com/example/birthdaysahead/model/Event.kt
+++ app/src/main/java/com/example/birthdaysahead/model/Event.kt
@@ -1,16 +1,28 @@
 package com.example.birthdaysahead.model
 
+import com.example.birthdaysahead.utils.formatListWithDash
+import com.example.birthdaysahead.utils.formatListWithNumbers
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
 
 data class Event(
     val name: String,
     val date: LocalDate,
     val color: Int,
     val typeOfEvent: TypeOfEvent,
-    val note: String,
-    val giftWishes: List<String>,
-    val likes: List<String>,
-    val dislikes: List<String>
+    val note: String? = null,
+    val giftWishes: List<String>? = emptyList(),
+    val likes: List<String>? = emptyList(),
+    val dislikes: List<String>? = emptyList()
 ) {
+    private val format = "dd/MM/yyyy"
+    private val formatter = DateTimeFormatter.ofPattern(format, Locale.getDefault())
+
     val iconChar: Char = name.first()
+    val dateString: String = formatter.format(date).toString()
+    val formattedWishes: String = giftWishes.formatListWithDash()
+    val formattedLikes: String = likes.formatListWithNumbers()
+    val formattedDislikes: String = dislikes.formatListWithNumbers()
 }
